e974b1f3799032e90efe2ed2db2a9a8fb2d7939e
Zack D <zsd7200@rit.edu>
2020-09-18 14:35:36 -0400

    confirmed: secure does not work

--- server/router.js
+++ server/router.js
@@ -1,9 +1,11 @@
 const controllers = require('./controllers');
-const mid = require('./middleware');
+// const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/', mid.requiresSecure, controllers.indexPage);
-  app.get('*', mid.requiresSecure, controllers.errPage);
+  // app.get('/', mid.requiresSecure, controllers.indexPage);
+  // app.get('*', mid.requiresSecure, controllers.errPage);
+  app.get('/', controllers.indexPage);
+  app.get('*', controllers.errPage);
 };
 
 module.exports = router;
