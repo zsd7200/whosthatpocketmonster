10559394f8acc3fc54306119f5fb65747fc164f5
Zack D <zsd7200@rit.edu>
2020-09-18 14:16:01 -0400

    see if removing requireSecure will fix the custom domain

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/', mid.requiresSecure, controllers.indexPage);
-  app.get('*', mid.requiresSecure, controllers.errPage);
+  app.get('/', controllers.indexPage);
+  app.get('*', controllers.errPage);
 };
 
 module.exports = router;
